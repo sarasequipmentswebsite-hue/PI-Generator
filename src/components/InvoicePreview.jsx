60374501d9941e98a076b7da829402a559cf4b5a
Developer <dev@example.com>
2026-04-22 14:36:51 +0530

    ....

--- src/components/InvoicePreview.jsx
+++ src/components/InvoicePreview.jsx
@@ -344,22 +344,12 @@ export default function InvoicePreview({ formData }) {
                     <td className="bot-left">
                       <div className="bot-decl-hdr">Declaration</div>
                       <p className="bot-decl-body">
-                      <br>
                         1. We declare that this invoice shows the actual price of the goods described
-                        and that all particulars are true and correct.
-                        </br>
-                        <br>
+                        and that all particulars are true and correct.<br />
                         2. No changes are valid after 15 days from the date mentioned in the Invoice.
-                        </br>
-                        <br>
                         3. Cheque returned unpaid from the bank are subjected to return charges Rs.500.
-                        </br>
-                        <br >
                         4. All Dispute subject to delhi juridiction.
-                        </br>
-                        <br>
                         5. Interest @24% P.A. will be charged if the payment is not made within the stipulated time.
-                        </br>
                       </p>
                       <div className="bot-pan"><u>Company's PAN:</u></div>
                       <div className="bot-pan-val"><em>{f.companyPan}</em></div>
@@ -386,7 +376,6 @@ export default function InvoicePreview({ formData }) {
                       </div>
                     </td>
                   </tr>
-                  
                 </tbody>
               </table>
             </>
